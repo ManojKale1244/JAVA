3410b0931bbde4edd28fb8803a2fa346576242fc
ManojKale1244 <manojkale187@gmail.com>
2026-01-07 21:12:52 +0530

    loop

---
+++ loop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class loop{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+      int a = 1;
+      System.out.print("Enter total print number :");
+      int num = sc.nextInt();
+
+      while(a<=num){
+        System.out.println("Hello World");
+        a++;
+
+      }
+  }
+}
