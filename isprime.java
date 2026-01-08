eb2c975924319c5316cb02cf09da793a03cd83d7
Manoj  Kale <manojkale187@gmail.com>
2026-01-08 13:27:30 +0530

    Correct prime number detection logic and output
    Fix logic for prime number check and correct print statements.

--- isprime.java
+++ isprime.java
@@ -5,7 +5,12 @@ public class isprime {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number :");
         int n = sc.nextInt();
-        boolean isPrime = true;
+        if(n==2){
+           System.ot.printin(" The given number is prime :" + n);
+        }
+
+        else{
+             boolean isPrime = true;
         for(int i =2;i<=n-1;i++){
            if (n%i==0) {
           isPrime = false;
@@ -21,6 +26,9 @@ public class isprime {
         else{
             System.out.println("The given number is not prime :" +n);
         }
+    
+        }
+      
         sc.close();
     }
 }
