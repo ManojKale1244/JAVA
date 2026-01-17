e508b3230a64f5a72cbbc347e0f6c4a97610722c
ManojKale1244 <manojkale187@gmail.com>
2026-01-17 19:13:53 +0530

    DecimalToBinary

--- binaryTodecimal.java
+++ binaryTodecimal.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class binaryTodecimal{
     public static void binTodeci(int n){
-        int pow = 0;
-        int decimal = 0;
+      int pow = 0;
+      int decimal = 0;
+       
         while (n>0) {
-            int LastDigit = n%10;
-            decimal = decimal + LastDigit * (int) Math.pow(2, pow);
-            pow++;
+          int LastDigit = n%10;
+          decimal = decimal + LastDigit * (int)(Math.pow(2, pow));
+          pow++; 
             n = n/10;
 
             
