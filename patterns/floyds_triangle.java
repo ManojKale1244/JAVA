cbd779cd6b88d16acf72be002901eb7f7f006375
ManojKale1244 <manojkale187@gmail.com>
2026-01-20 21:01:21 +0530

    number Triangle

--- patterns/floyds_triangle.java
+++ patterns/floyds_triangle.java
@@ -1,24 +1,27 @@
 import java.util.*;
 
-public class InHalf_pyramid {
+public class floyds_triangle {
     public static void Half_Pyramid(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(j);
+        int count = 1;
+        for(int i =1;i<=n;i++){
+              
+            for(int j =1;j<=i;j++){
+               
+                System.out.print(count + " ");
+                count++;
             }
-            for (int k = 1; k <= i; k++) {
+            for(int k =1;k<=n-i;k++){
                 System.out.print(" ");
             }
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number :");
+    System.out.print("Enter your numbber :");
+
         int Num = sc.nextInt();
         Half_Pyramid(Num);
         sc.close();
-
     }
 }
