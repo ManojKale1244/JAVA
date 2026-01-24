b70a156e572e4c2abe15766e97237a1aad164678
ManojKale1244 <manojkale187@gmail.com>
2026-01-25 00:49:21 +0530

    Food array

--- Array/LinearSearch.java
+++ Array/LinearSearch.java
@@ -1,26 +1,27 @@
 import java.util.*;
+
 public class LinearSearch {
-    public static int Linear(int number[],int key){
-        for(int i =0;i<number.length;i++){
-            if(number[i] == key){
+    public static int Linear(String number[], String key) {
+        for (int i = 0; i < number.length; i++) {
+            if (number[i].equals(key)) {
                 return i;
             }
         }
-return -1;
+        return -1;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number[] = {2,4,6,8,10,12,14,16};
-        System.out.print("Enter a key value :");
-        int key =sc.nextInt();
-        int index = Linear(number,key);
-        if(index == -1){
+        String number[] = { "Vadapav", "bhel", "samosa" };
+        System.out.print("Enter a value :");
+        String key = sc.nextLine();
+        int index = Linear(number, key);
+        if (index == -1) {
             System.out.println("Not found Key");
-        }
-        else{
+        } else {
             System.out.println("Key is at index : " + index);
         }
-
+        sc.close();
 
     }
 }
