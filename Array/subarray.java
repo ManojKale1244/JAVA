def0481254f0ffcd30ecd4946997eb5dbb606999
ManojKale1244 <manojkale187@gmail.com>
2026-01-27 16:30:51 +0530

    SubArray

---
+++ Array/subarray.java
@@ -0,0 +1,21 @@
+public class subarray {
+    public static void SubaArr(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            int start = i;
+            for(int j=0; j<arr.length;j++){
+                int end = j;
+                for(int k = start;k<=end;k++){
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        SubaArr(arr);
+        
+    }
+}
