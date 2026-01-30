e6f969752749ba8d6657283c68b78e42ec6167da
ManojKale1244 <manojkale187@gmail.com>
2026-01-30 22:10:26 +0530

    Bubble Sort

---
+++ Array/Binary.java
@@ -0,0 +1,26 @@
+public class Binary {
+    public static int ArrNum(int arr[], int key){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<= end){
+          int mid = (start+end)/2;
+          if(arr[mid] == key){
+            return mid;
+          }
+          if(arr[mid]<key){
+            start = mid+1;
+          }
+          else{
+            end = mid-1;
+          }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args){
+        int arr[] = {4,  5, 6, 7, 0, 1, 2};
+        int key = 3;
+        ArrNum(arr , key);
+
+    }
+}
