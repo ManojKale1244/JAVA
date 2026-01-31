9893d4b0680a8935cc0d37a8fbd6ce5a3ead9ac5
ManojKale1244 <manojkale187@gmail.com>
2026-01-31 13:57:13 +0530

    SelectionSort

--- Array/BubbleSort.java
+++ Array/BubbleSort.java
@@ -1,23 +1,33 @@
 public class BubbleSort {
     public static void BubbleSorts(int arr[]){
-        for(int i = 0; i<arr.length-1;i++){
-            for(int j = 0;j<arr.length-1-i;j++){
-                if(arr[j] > arr[j+1]){
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
+        for(int i =0; i<arr.length-1;i++){
+            int swap = 0;
+            for(int j=0;j<arr.length-1-i;j++){
+                if(swap == j){
+                    if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                 
                 }
+                swap ++; 
+                }
+              
+                
                 
             }
+              
         }
+        System.out.print("sorted array :");
 
     }
     public static void main(String[] args){
-        int arr[] = {5,4,1,3,2};
+        int arr[] = {1,2,3,4,5,6};
         BubbleSorts(arr);
-        for(int i=0;i<arr.length;i++){
+        for(int i =0 ; i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
+       
     }
 }
 
