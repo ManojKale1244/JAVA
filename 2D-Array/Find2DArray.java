a045b8e845d0e1fbdd9386e82651dc89cb91ab9f
ManojKale1244 <manojkale187@gmail.com>
2026-02-11 21:37:42 +0530

    Search 2D array

--- 2D-Array/Find2DArray.java
+++ 2D-Array/Find2DArray.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Matrices {
+public class Find2DArray {
     public static boolean Search(int arr[][],int key){
          int n = arr.length;
         int m = arr[0].length;
