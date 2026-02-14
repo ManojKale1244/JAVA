b66450e074f16a3fa8fc3ce19ebe02a42b8df2d7
ManojKale1244 <manojkale187@gmail.com>
2026-02-14 16:40:13 +0530

    find duplicate eliment

---
+++ Array/Duplicate.java
@@ -0,0 +1,23 @@
+public class Duplicate {
+    public static void DuplicateFind(int arr[]){
+       int i =0;
+       
+       for(int j=1;j<arr.length;j++){
+       if(arr[j] != arr[i]){
+        arr[i+1] = arr[j];
+        
+         System.out.println(arr[i]);
+         i++;
+       }
+
+        }
+      
+     
+       
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,3,4,4};
+        DuplicateFind(arr);
+       
+    }
+}
