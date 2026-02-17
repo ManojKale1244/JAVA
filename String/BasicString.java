c5fc24e1951c567fc2d8398ed5087c9156b18bf5
ManojKale1244 <manojkale187@gmail.com>
2026-02-17 21:50:44 +0530

    to Find Charator

--- String/BasicString.java
+++ String/BasicString.java
@@ -1,9 +1,14 @@
 import java.util.*;
 public class BasicString {
+    public static void string(String name){
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name :");
         String name = sc.nextLine();
-        System.out.println(name.length());
+        string(name);
     }
 }
