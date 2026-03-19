091243d80dfd536817538ec73d42e2fc1a054cf2
ManojKale1244 <manojkale187@gmail.com>
2026-03-19 13:41:32 +0530

    Que_1

---
+++ OOPs/Que_1.java
@@ -0,0 +1,13 @@
+public class Que_1 {
+    public static void main(String[] args) {
+        Student S = new Student();
+        S.name = "Manoj";
+        System.out.println(S.name);
+        
+    }
+  
+}
+  class Student{
+    String name;
+    int marks;
+  }
