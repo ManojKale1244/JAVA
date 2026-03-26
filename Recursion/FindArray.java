7f483215535a7a8cfe31ad2d36f6f90262ef2bd6
ManojKale1244 <manojkale187@gmail.com>
2026-03-26 14:52:46 +0530

    Find Array Element

---
+++ Recursion/FindArray.java
@@ -0,0 +1,16 @@
+public class FindArray {
+    public static int FindArry(int arr[],int i,int key){
+        if(i == arr.length){
+            return -1;
+        }
+
+        if(arr[i] == key){
+            return 1;
+        }
+        return FindArry(arr, i+1, key);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,4,7,3,9};
+      System.out.println(FindArry(arr, 0, 5));
+    }
+}
