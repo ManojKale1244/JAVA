ba7a9733b3dfcc258be22e91adc8bb6c29d31044
ManojKale1244 <manojkale187@gmail.com>
2026-03-31 22:14:31 +0530

    FriendPairs

---
+++ Recursion/FriendPair.java
@@ -0,0 +1,11 @@
+public class FriendPair {
+    public static int Pair(int n){
+     if(n==1|| n==2){
+        return n;
+     }
+   return Pair(n-1) + (n-1) * Pair(n-2);
+    }
+    public static void main(String[] args) {
+       System.out.println(Pair(4));
+    }
+}
