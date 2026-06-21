1b7a1ebc6e899dc36303c66677271f2c0ea44d61
Manoj  Kale <manojkale187@gmail.com>
2026-06-21 13:42:07 +0530

    Linked List

--- LinkedList/Basic1.java
+++ LinkedList/Basic1.java
@@ -1,22 +1,23 @@
+import java.util.*;
 
-import java.util.LinkedList;
-
-public class Basic {
-    public static class Node{
+public class Basic1 {
+   static class Node{
         int data;
         Node next;
-
         public Node(int data){
             this.data = data;
             this.next = null;
         }
+
     }
     public static Node head;
     public static Node tail;
 
-    public static void main(String[] args) {
-        Basic ll = new Basic();
+    public static void main(String[] args){
+        Basic1 ll = new Basic1();
         ll.head = new Node(1);
         ll.head.next = new Node(2);
+
     }
+    
 }
