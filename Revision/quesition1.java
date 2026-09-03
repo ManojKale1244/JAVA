cfad5383c7ddc7f5a32a9e45bdab9a7ef4714df5
Manoj  Kale <manojkale187@gmail.com>
2026-09-03 22:33:17 +0530

    push the code

---
+++ Revision/quesition1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class quesition1{
+    public static void main(String [] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter a first number :");
+   int a = sc.nextInt();
+   System.out.print("Enter a Second number :");
+   int b = sc.nextInt();
+
+   int sum = a + b;
+
+   System.out.println("The Totle sum is: " + sum);
+    }
+
+}
